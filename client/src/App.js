0524c964276b6a3c943cb4a26fc238d24b8bc249
Viktor <topcoder@rambler.ru>
2023-01-05 19:57:39 +0300

    CRA setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './cruiser-military.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          SEA BATTLE
+        </h1>
+      </header>
+      <main>
+        <button>Enter</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
